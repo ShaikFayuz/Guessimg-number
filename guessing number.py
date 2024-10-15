4d040a2bc6caa6046fc0e369ba4ea74b4f841f1b
ShaikFayuz <fayuzahmed@gmail.com>
2024-10-15 08:12:49 +0530

    Add files via upload

---
+++ guessing number.py
@@ -0,0 +1,13 @@
+import random
+
+guess = 0
+randNum = random.randrange(1,11)
+
+while guess != randNum:
+    guess = int(input("Try to guess your number:"))
+    if guess == randNum:
+        print("You got it")
+    if (guess > randNum):
+        print("Wrong Your guessing number is High")
+    if (guess < randNum):
+        print("Wrong, Your guessing number is low")
